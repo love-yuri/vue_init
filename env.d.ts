5a61c11bc1fa91fe45755e7ba8a5299975444d52
love-yuri <yuri2078170658@gmail.com>
2024-04-11 23:11:17 +0800

    feat: 添加.env 和 env.d.ts 文件以支持 VITE环境变量

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-04-11 22:51:42
+ * @LastEditTime: 2024-04-11 22:56:28
+ * @Description: vite env 环境变量
+ */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENV_TEST: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

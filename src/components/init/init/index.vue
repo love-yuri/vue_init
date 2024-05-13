2524336444ac1fc6714792a3b38636623988650f
love-yuri <yuri2078170658@gmail.com>
2024-05-13 20:21:45 +0800

    feat: 更新 init 组件中的按钮样式和文本

--- src/components/init/init/index.vue
+++ src/components/init/init/index.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-04-11 22:13:13
- * @LastEditTime: 2024-04-12 21:58:11
+ * @LastEditTime: 2024-05-13 20:19:50
  * @Description: 基础模板
 -->
 <template>
   <div class="flex items-center p-2">
     <el-button type="primary" class="mx-2"> {{ text }} </el-button>
-    <Button class="yuri-ant">这是 vue-init</Button>
+    <Button>这是 vue-init</Button>
   </div>
 </template>
 <script setup lang="ts">

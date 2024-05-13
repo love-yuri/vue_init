62ec05f3c651cc1c092cb211150741afbaebbbec
love-yuri <yuri2078170658@gmail.com>
2024-05-13 21:55:47 +0800

    feat: 登录api测试

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-04-11 23:06:51
- * @LastEditTime: 2024-04-12 21:55:25
+ * @LastEditTime: 2024-05-13 21:53:42
  * @Description: 欢迎界面
 -->
 <template>
@@ -11,4 +11,11 @@
 </template>
 <script setup lang="ts">
 import { InitComponents } from '@/components/init/index';
+import { login } from '@/api/userApi/user';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const res = await login('');
+  console.log(res);
+});
 </script>
